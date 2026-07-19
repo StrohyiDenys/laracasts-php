ca3c5b37e6a43b397cf48648e5b03c17a8d66704
@StrohyiDenys <falonad@gmail.com>
2026-07-19 23:23:37 +0300

    Lesson 27

--- src/controllers/note-create.php
+++ src/controllers/note-create.php
@@ -3,9 +3,18 @@ $config = require ('config.php');
 $db = new Database($config['database'], 'root');
 $heading = "Notes";
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
-    $db->Query("INSERT INTO notes (body, user_id) VALUES(:body, :user_id)", [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+    $errors = [];
+    if (strlen($_POST['body']) == 0){
+        $errors['body'] = "You cant save empty note";
+    }
+    if(strlen($_POST['body']) > 2000){
+        $errors['body'] = "Error";
+    }
+    if(empty($errors)){
+        $db->Query("INSERT INTO notes (body, user_id) VALUES(:body, :user_id)", [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 require "views/note-create.view.php";
--- src/views/note-create.view.php
+++ src/views/note-create.view.php
@@ -17,8 +17,11 @@
                             <div class="col-span-full block ">
                                 <label for="body" class="block text-sm/6 font-medium text-gray-900">Note</label>
                                 <div class="mt-2">
-                                    <textarea id="body" name="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+                                    <textarea id="body" name="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"><?= isset($_POST['body']) && !empty($errors['body']) ? $_POST['body'] : '' ?></textarea>
                                 </div>
+                                <?php if (isset($errors['body'])):?>
+                                <div class="text-red-500 text-sm"><?=$errors['body']?></div>
+                                <?php endif?>
                             </div>
 
                             <div class="mt-4 flex items-center justify-end gap-x-6">
